4c4b8b2c90e47593b336d53b7ff47062469397ec
Edvinas Pranka <epranka@develop.localhost>
2020-07-07 20:15:14 +0300

    Added Docker

---
+++ Dockerfile
@@ -0,0 +1,13 @@
+FROM node:14.5.0-slim
+
+WORKDIR /usr/src/app
+
+COPY package*.json ./
+
+RUN npm install
+
+COPY . ./
+
+RUN npm run build --prod
+
+CMD ["npm", "run", "server:start"]
--- server.js
+++ server.js
@@ -5,6 +5,6 @@ const port = process.env.PORT || 8080;
 
 app.use(express.static("dist/currency-exchange-app"));
 
-app.listen(port, () => {
+app.listen(port, "0.0.0.0", () => {
   console.log("Application is listening on port", port);
 });
